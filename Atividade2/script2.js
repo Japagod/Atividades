d4edc02677100abbc6715679e5195010f0400001
Japagod <80219032+Japagod@users.noreply.github.com>
2021-10-15 16:14:32 -0300

    Add files via upload

--- Atividade2/script2.js
+++ Atividade2/script2.js
@@ -9,6 +9,23 @@ function menor(){
      document.getElementById("texto1").innerHTML.small()
 }
 // Parte 2
+function Blue(){
+    document.getElementById("texto2").style.color = "blue"
+}
+function Red(){
+    document.getElementById("texto2").style.color = "red"
+}
+function Green(){
+    document.getElementById("texto2").style.color = "green"
+}
 function cor(){
+    let cor = document.getElementById("cor").value
+    document.getElementById("texto2").style.color = cor
+}
+function mais(){
     
+    let res = document.getElementById("num")
+    res.innerHTML = num.value + 1
+    console.debug(res)
+      
 }
