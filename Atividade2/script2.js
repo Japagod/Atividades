c3808b955b9f0bd7734ee749a14efc4635530be9
Japagod <80219032+Japagod@users.noreply.github.com>
2021-10-15 16:57:06 -0300

    Add files via upload

--- Atividade2/script2.js
+++ Atividade2/script2.js
@@ -22,10 +22,49 @@ function cor(){
     let cor = document.getElementById("cor").value
     document.getElementById("texto2").style.color = cor
 }
+var i = 0
 function mais(){
     
     let res = document.getElementById("num")
-    res.innerHTML = num.value + 1
+    if(i<10)
+    i++
+    res.innerHTML = i
     console.debug(res)
       
 }
+function menos(){
+
+    let res = document.getElementById("num")
+    if(i>0)
+    i--
+    res.innerHTML = i
+    console.debug(res)
+
+}
+//Parte 4
+function calc() {
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+    var c = document.getElementById("c").value;
+
+    var delta = b * b + 4 * a * c;
+    console.log(delta);
+    if (delta > 0) {
+      var x1 = -b + (Math.sqrt(delta) / 2) * a;
+      var x2 = -b - (Math.sqrt(delta) / 2) * a;
+      document.getElementById(
+        "result"
+      ).innerHTML = `Delta igual a ${delta}, função tem raizes iguais à ${x1} e ${x2}`;
+    } else {
+      if ((delta = 0)) {
+        var x1 = -b + (Math.sqrt(delta) / 2) * a;
+        document.getElementById(
+          "result"
+        ).innerHTML = `Delta igual a ${delta}, então função tem duas raizes iguais a ${x1}`;
+      } else {
+        document.getElementById(
+          "result"
+        ).innerHTML = `Delta igual a ${delta}, então função não possui raizes reais`;
+      }
+    }
+  }
