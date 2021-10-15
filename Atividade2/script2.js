17336733c9e0a0094527aef81d39fe2fc4e69c70
Japagod <80219032+Japagod@users.noreply.github.com>
2021-10-15 01:33:41 -0300

    Add files via upload

---
+++ Atividade2/script2.js
@@ -0,0 +1,14 @@
+//Parte 1
+function maior(){
+    document.getElementById("texto1").innerHTML = 
+     document.getElementById("texto1").innerHTML.big
+     ();
+}
+function menor(){
+    document.getElementById("texto1").innerHTML = 
+     document.getElementById("texto1").innerHTML.small()
+}
+// Parte 2
+function cor(){
+    
+}
